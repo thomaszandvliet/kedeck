cc4ee80d99c3495b4e005a6fd33d913786a3fe9d
ThomasOtte <thomas.otte@itvitaelearning.onmicrosoft.com>
2016-08-15 13:50:36 +0200

    Particles

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -21,6 +21,6 @@ public class EnemyManager : MonoBehaviour {
 		float spawnY = Random.Range(((-Camera.main.ScreenToWorldPoint (new Vector2 (0, Screen.height)).y) + 0.6f), (Camera.main.ScreenToWorldPoint (new Vector2 (0, Screen.height)).y - 0.6f));
 		Vector2 spawnPosition = new Vector2 (Camera.main.ScreenToWorldPoint (new Vector2 (Screen.width, 0)).x + 7, spawnY);
 
-		Instantiate (enemyOne, spawnPosition, Quaternion.identity);
+		Instantiate (enemyOne, spawnPosition, Quaternion.Euler(0,0,270));
 	}
 }
