33e6cf2df712c5229a87abc61c3bbd0c52ff1646
Michael Rensen <michael@illi-engineering.nl>
2016-08-17 21:52:29 +0200

    EnemyTwo added, needs spawing

--- Assets/EnemyTwo.cs
+++ Assets/EnemyTwo.cs
@@ -3,47 +3,73 @@ using System.Collections;
 
 public class EnemyTwo : MonoBehaviour {
 
+	float speed;
+	float hastouchedWall = 1f;
 	private Rigidbody2D rb2d;
 
-	// Use this for initialization
 	void Start () {
-
+		speed = 5f;
 	}
 
-	void Awake() {
+	void Awake () {
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 
+
+
 	// Update is called once per frame
-	void Update () {
-		//Instantiate (gameObject, spawnPosition.x, spawnPosition.y);
+	void FixedUpdate () {
+
+		goMove();
+//		Vector3 temp = new Vector3 (200f,0f);
+//		rb2d.transform.position -= temp;
+
 	}
 
-	void FixedUpdate() {
+	void goMove() {
+		transform.position += Vector3.down * speed * Time.deltaTime;
 		Vector3 temp = new Vector3 (0.05f,0f);
 		rb2d.transform.position -= temp;
 	}
+		
 
 	void OnCollisionEnter2D(Collision2D col) {
 		if(col.gameObject.tag == "Player") {
 			//Destroy (gameObject); // Destroy Enemy on Collision with Player
 		}
+
 		if (col.gameObject.tag == "EnemyDestroyer") {
 			Destroy (gameObject);
 		}
 
-		if (col.gameObject.tag == "Wall") {
-			Debug.Log ("HOER");
-			Vector3 temp = new Vector3 (0f, -0.4f);
-			rb2d.transform.position -= temp;
-		} else {
-			Debug.Log ("HOERtje");
-			Vector3 temp = new Vector3 (0f, 0.4f);
-			rb2d.transform.position -= temp;
-			
+		if (col.gameObject.tag == "WallUp") {
+			speed = 5f;
+			hastouchedWall = 0f;
+			Debug.Log (hastouchedWall);
 		}
 
-
+		if (col.gameObject.tag == "WallDown") {
+			speed = -5f;
+			hastouchedWall = 1f;
+			Debug.Log (hastouchedWall);
+		}
 	}
+		
+//	void OnCollisionExit2D(Collision2D col) {
+//		if (col.gameObject.tag == "Wall" && hastouchedWall == 1f) {
+//			speed = 5f;
+//			hastouchedWall = 0f;
+//			Debug.Log (hastouchedWall);
+//		}
+//
+//		if (col.gameObject.tag == "Wall" && hastouchedWall == 0f) {
+//			speed = -5f;
+//			hastouchedWall = 1f;
+//			Debug.Log (hastouchedWall);
+//		}
+//
+//
+//	}
+
 }
 
