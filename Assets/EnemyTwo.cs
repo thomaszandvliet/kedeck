277b5d9ecd60371b7689ddffbb348454315a1ec7
Michael Rensen <michael@illi-engineering.nl>
2016-08-17 16:58:59 +0200

    Begin aan Enemy2

---
+++ Assets/EnemyTwo.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyTwo : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Awake() {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Instantiate (gameObject, spawnPosition.x, spawnPosition.y);
+	}
+
+	void FixedUpdate() {
+		Vector3 temp = new Vector3 (0.05f,0f);
+		rb2d.transform.position -= temp;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag == "Player") {
+			//Destroy (gameObject); // Destroy Enemy on Collision with Player
+		}
+		if (col.gameObject.tag == "EnemyDestroyer") {
+			Destroy (gameObject);
+		}
+
+		if (col.gameObject.tag == "Wall") {
+			Debug.Log ("HOER");
+			Vector3 temp = new Vector3 (0f, -0.4f);
+			rb2d.transform.position -= temp;
+		} else {
+			Debug.Log ("HOERtje");
+			Vector3 temp = new Vector3 (0f, 0.4f);
+			rb2d.transform.position -= temp;
+			
+		}
+
+
+	}
+}
+
