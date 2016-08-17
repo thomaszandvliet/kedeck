d4e44ea5dd281d8cdeb218b2df1b5ce835875bd0
ThomasOtte <thomas.otte@itvitaelearning.onmicrosoft.com>
2016-08-18 00:01:40 +0200

    PauseButton Fix

--- Assets/Scripts/PauseButton.cs
+++ Assets/Scripts/PauseButton.cs
@@ -4,46 +4,40 @@ using UnityEngine.UI;
 
 public class PauseButton : MonoBehaviour {
 
-	public Sprite pauseSprite;
-	public Sprite playSprite;
+    private SpriteRenderer spriteRenderer;
 
+    public Sprite pauseSprite;
+    public Sprite playSprite;
 
-	public Sprite spriteToChangeTo;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sprite == null) 
+            spriteRenderer.sprite = pauseSprite;
 
-	public void PauseGame () {
-		if (Time.timeScale == 1) {
-			Time.timeScale = 0;
-		} else if (Time.timeScale == 0) {
-			Time.timeScale = 1;
-		}
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-		
 
-	public void ChangeImage(Image myImageToUpdate)
-	{
-		myImageToUpdate.sprite = spriteToChangeTo;
-	}
+    void OnMouseDown()
+    {
+        ChangeImage();
+    }
 
-//	public void ChangeImage(Image myImageToUpdate)
-//	{
-//		if (originalSprite == !spriteToChangeTo) {
-//			myImageToUpdate.sprite = spriteToChangeTo;
-//			Debug.Log ("1");
-//		} else if (spriteToChangeTo == !originalSprite) {
-//			myImageToUpdate.sprite = originalSprite;
-//			Debug.Log ("2");
-//		}
-//	}
-
-
-			
+    public void ChangeImage()
+    {
+        if (spriteRenderer.sprite == pauseSprite) 
+        {
+            spriteRenderer.sprite = playSprite;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            spriteRenderer.sprite = pauseSprite; 
+            Time.timeScale = 1;
+        }
+    }		
 }
